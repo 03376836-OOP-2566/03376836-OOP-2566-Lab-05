3852d5e9975b1b57c22d5c7c82ac6267b8eba06e
SitthaKlaphanich <ao2604152014@hotmail.com>
2024-02-27 12:18:13 +0700

    Lab05_Exercise05_result.md

---
+++ Lab05-0258/Lab05_Ex05/Program.cs
@@ -0,0 +1,19 @@
+﻿var myObj = new MyClass();
+myObj.Info();
+
+class MyClass
+{
+    int F1;
+    string F2;
+    int F3 = 100;
+    string F4 = "ASDF";
+
+    public void Info()
+    {
+        System.Console.WriteLine($"Default integer      F1 = {F1}");  //Implicit fields initialization
+        System.Console.WriteLine($"Default string       F2 = {F2}");
+
+        System.Console.WriteLine($"Initialized integer  F3 = {F3}");  //Explicit field initialization
+        System.Console.WriteLine($"Initialized string   F4 = {F4}");
+    } 
+}
