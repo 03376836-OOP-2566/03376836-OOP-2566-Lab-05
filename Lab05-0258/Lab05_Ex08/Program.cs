cdc74471c17bb4363846ace2992a53fb41d1826f
SitthaKlaphanich <ao2604152014@hotmail.com>
2024-02-27 12:44:36 +0700

    Lab05_Exercise08_result.md

---
+++ Lab05-0258/Lab05_Ex08/Program.cs
@@ -0,0 +1,19 @@
+﻿int x = 2, y = 3;
+int add_result = Calculator.add(x, y);
+System.Console.WriteLine($"{x} + {y} = {add_result}");
+
+int i = 2, j = 3;
+int multiply_result = Calculator.multiply(i, j);
+System.Console.WriteLine($"{i} x {j} = {multiply_result}");
+
+public static class Calculator
+{
+    public static int add(int a, int b)
+    {
+        return a + b;
+    }
+    public static int multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
