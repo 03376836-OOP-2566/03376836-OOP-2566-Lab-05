837e67ca1f151fef29dc935085810d9e40692a31
Sorawit255 <65030255@kmitl.ac.th>
2024-03-06 19:27:34 +0700

    6

---
+++ 0255/Lab05_Ex06/Program.cs
@@ -0,0 +1,9 @@
+﻿var mc = new MyClass();
+mc.DisplayParam(3, 4);
+class MyClass
+{
+    public void DisplayParam(int a, int b)
+    {
+        System.Console.WriteLine($"a = {a}, b = {b}");
+    } 
+}
