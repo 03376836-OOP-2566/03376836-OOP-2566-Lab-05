7f517ddcbac8d68147ab585b82704be567735825
AnchisaPhetnoi <65030289@kmitl.ac.th>
2024-03-22 01:25:31 +0700

    03376836-OOP-2566-Lab-05

--- Lab05_Ex06/Program.cs
+++ Lab05_Ex06/Program.cs
@@ -1,10 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-var mc = new MyClass();
-mc.DisplayParam(3, 4);
-class MyClass
+var adder = new MyProgram();
+int x = 2, y = 3;
+int add_result = adder.add(x, y);
+System.Console.WriteLine($"{x} + {y} = {add_result}");
+
+int i = 2, j = 3;
+int multiply_result = adder.multiply(i, j);
+System.Console.WriteLine($"{i} x {j} = {multiply_result}");
+
+class MyProgram
 {
-    public void DisplayParam(int a, int b)
+    public int add(int a, int b)
     {
-        System.Console.WriteLine($"a = {a}, b = {b}");
-    } 
+        return a + b;
+    }
+    public int multiply(int a, int b)
+    {
+        return a * b;
+    }
 }
