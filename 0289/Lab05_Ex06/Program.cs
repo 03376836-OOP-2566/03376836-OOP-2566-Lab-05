3790e1bbde2001dd153cb691dd1d6a4cb4a8cd9a
AnchisaPhetnoi <65030289@kmitl.ac.th>
2024-03-22 16:07:01 +0700

    03376836-OOP-2566-Lab-05

---
+++ 0289/Lab05_Ex06/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+var mc = new MyClass();
+mc.DisplayParam(3, 4);
+class MyClass
+{
+    public void DisplayParam(int a, int b)
+    {
+        System.Console.WriteLine($"a = {a}, b = {b}");
+    } 
+}
