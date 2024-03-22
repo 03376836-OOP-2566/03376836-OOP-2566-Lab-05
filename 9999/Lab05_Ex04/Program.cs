4578c6c62927e88a8795002ef50500b9e04e4465
fulk2014 <144195753+fulk2014@users.noreply.github.com>
2024-03-22 15:19:26 +0700

    lab_05_65030275

---
+++ 9999/Lab05_Ex04/Program.cs
@@ -0,0 +1,14 @@
+﻿var myObj = new MyClass();
+
+System.Console.WriteLine($"Default integer      F1 = {myObj.F1}");  //Implicit fields initialization
+System.Console.WriteLine($"Default string       F2 = {myObj.F2}");
+System.Console.WriteLine($"Initialized integer  F3 = {myObj.F3}");  //Explicit field initialization
+System.Console.WriteLine($"Initialized string   F4 = {myObj.F4}");
+
+class MyClass
+{
+    public int F1;
+    public string F2;
+    public int F3 = 100;
+    public string F4 = "ASDF";
+}
---
+++ Lab_result/lab05_05.md
@@ -0,0 +1,3 @@
+![alt text](image-6.png)
+
+![alt text](image-7.png)
