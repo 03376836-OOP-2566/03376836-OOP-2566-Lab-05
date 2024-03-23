eab4d21d9de70c0b8cd5dd9b31b714d47b682613
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-23 23:02:08 +0700

    commit Ex06

--- 65030179_Lab05/Lab05_Ex06/Program.cs
+++ 65030179_Lab05/Lab05_Ex06/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var mc = new MyClass();
+mc.DisplayParam(3, 4);
+class MyClass
+{
+    public void DisplayParam(int a, int b)
+    {
+        System.Console.WriteLine($"a = {a}, b = {b}");
+    } 
+}
