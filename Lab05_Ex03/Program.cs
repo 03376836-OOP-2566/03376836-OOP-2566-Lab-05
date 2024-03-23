f14b8903ffe8b5812578fbad6d94acf946d30b54
newyork <spidermanwin1@gmail.com>
2024-03-24 05:04:11 +0700

    ex3

---
+++ Lab05_Ex03/Program.cs
@@ -0,0 +1,15 @@
+﻿Person p = new Person();
+p.name = "Rambo";
+p.id = "1987";
+p.income = 2500;
+System.Console.WriteLine($"Type of p is {p.GetType()}");
+System.Console.WriteLine($"p.name = {p.name}\ttype = {p.name.GetType()}");
+System.Console.WriteLine($"p.id = {p.id}\ttype = {p.id.GetType()}");
+System.Console.WriteLine($"p.income = {p.income}\ttype = {p.income.GetType()}");
+
+class Person
+{
+    public string? name;
+    public string? id;
+    public int income;
+}
