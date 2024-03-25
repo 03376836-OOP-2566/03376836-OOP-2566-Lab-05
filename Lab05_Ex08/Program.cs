7f16a4bcce55e1ef2a9b2693301c41a92e168730
Khemmasedth <khmasade555@gmail.com>
2024-03-25 23:55:39 +0700

    finish

---
+++ Lab05_Ex08/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int x = 2, y = 3;
+int add_result = Calculator.add(x, y);
+System.Console.WriteLine($"{x} + {y} = {add_result}");
+
+int i = 2, j = 3;
+int multiply_result = Calculator.multiply(i, j);
+System.Console.WriteLine($"{i} x {j} = {multiply_result}");
+
+public static class Calculator
+{
+    public static int add(int a, int b)
+    {
+        return a + b;
+    }
+    public static int multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
