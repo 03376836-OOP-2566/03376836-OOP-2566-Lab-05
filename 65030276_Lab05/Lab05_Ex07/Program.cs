2f7c269e394d41f17e10db9c8c359e8100c62a68
anndyyz <anda2546r@gmail.com>
2024-03-27 04:35:41 +0700

    ex7

---
+++ 65030276_Lab05/Lab05_Ex07/Program.cs
@@ -0,0 +1,20 @@
+﻿var adder = new MyProgram();
+int x = 2, y = 3;
+int add_result = adder.add(x, y);
+System.Console.WriteLine($"{x} + {y} = {add_result}");
+
+int i = 2, j = 3;
+int multiply_result = adder.multiply(i, j);
+System.Console.WriteLine($"{i} x {j} = {multiply_result}");
+
+class MyProgram
+{
+    public int add(int a, int b)
+    {
+        return a + b;
+    }
+    public int multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
